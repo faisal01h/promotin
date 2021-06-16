4803c210e3addbcb0314dd44e444a38233f3d375
Faisal Hanif <faisal01h@gmail.com>
2021-06-16 08:19:28 +0700

    auth

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.scss';
+import AuthenticationService from '../auth';
+import { Button } from '../../components/atoms';
+
+function sendLoginData() {
+    console.log("attempting to login...");
+    AuthenticationService.signin("test", "yes");
+}
+
+if(AuthenticationService.getCurrentUser()) {
+    console.log("Already logged in");
+    // redirect jal
+}
+else console.log("require log in");
+
+//TODO: Button onclick, panggil AuthenticationService.signin
 
 const Login = () => {
-    
+
     return (
         <div className="login-page-wrapper">
             <div className="login-card">
                 <div className="form-title">
                     <h1>Masuk</h1>
                 </div>
-                <div class="inner-form">
+                <div className="inner-form">
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
                         <input type="email" name="email" className="input-form" />
@@ -19,7 +34,10 @@ const Login = () => {
                         <input type="password" name="password" className="input-form" />
                     </div>
                     <div className="form-options">
-                        <button name="loginBtn" class="btn">Masuk</button>
+                        <Button 
+                            title={"Masuk"}
+                            onClick={sendLoginData}
+                        />
                         <a href="#">Lupa kata sandi?</a>
                     </div>
                 </div>
